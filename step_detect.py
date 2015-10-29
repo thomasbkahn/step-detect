0bc447e8ac35cf20b1d0f6097d1a2f0491937649
thomasbkahn <thomas.b.kahn@gmail.com>
2015-10-28 22:17:55 -0400

    Added docstrings to step_detect.py (and made several small adjustments) and elaborated in README.md

--- step_detect.py
+++ step_detect.py
@@ -33,14 +33,14 @@ def t_scan(L, window = 1e3):
         statistic calculation cannot be performed in that case.
 
     """
-    size     = L.size
-    window   = int(window)
-    frames   = range(window)
-    n_cols   = (size / window) - 1
+    size    = L.size
+    window  = int(window)
+    frames  = range(window)
+    n_cols  = (size / window) - 1
     
     t_stat  = np.zeros((window, n_cols))
     pool    = mp.Pool(processes = mp.cpu_count() - 1)
-    results = [pool.apply_async(t_scan_drone, args=(L, n_cols, frame, window)) for frame in frames]
+    results = [pool.apply_async(_t_scan_drone, args=(L, n_cols, frame, window)) for frame in frames]
     results = [r.get() for r in results]
     
     for index, row in results:
@@ -51,7 +51,13 @@ def t_scan(L, window = 1e3):
     return t_stat
 
 
-def t_scan_drone(L, n_cols, frame, window=1e3):
+def _t_scan_drone(L, n_cols, frame, window=1e3):
+    """
+    Drone function for t_scan. Not Intended to be called manually.
+    Computes t_scan for the designated frame, and returns result as
+    array along with an integer tag for proper placement in the 
+    aggregate array
+    """
     size   = L.size
     window = int(window)
     root_n = sqrt(window)
@@ -71,7 +77,32 @@ def t_scan_drone(L, n_cols, frame, window=1e3):
 
 
 def mz_fwt(x, n=2):
-    N_pnts = x.size
+    """
+    Computes the multiscale product of the Mallat-Zhong discrete forward
+    wavelet transform up to and including scale n for the input data x.
+    If n is even, the spikes in the signal will be positive. If n is odd
+    the spikes will match the polarity of the step (positive for steps
+    up, negative for steps down).
+
+    This function is essentially a direct translation of the MATLAB code
+    provided by Sadler and Swami in section A.4 of the following:
+    http://www.dtic.mil/dtic/tr/fulltext/u2/a351960.pdf
+
+    Parameters
+    ----------
+    x : numpy array
+        1 dimensional array that represents time series of data points
+    n : int
+        Highest scale to multiply to
+
+
+    Returns
+    -------
+    prod : numpy array
+        The multiscale product for x
+
+    """
+    N_pnts   = x.size
     lambda_j = [1.5, 1.12, 1.03, 1.01][0:n]
     if n > 4:
         lambda_j += [1.0]*(n-4)
@@ -86,39 +117,66 @@ def mz_fwt(x, n=2):
         Gn.append(q+1)
         Hn.append(3*q+1)
 
-    S = np.append(x[::-1], x)
-    S = np.append(S, x[::-1])
+    S    = np.append(x[::-1], x)
+    S    = np.append(S, x[::-1])
     prod = np.ones(N_pnts)
     for j in range(n):
         n_zeros = 2**j - 1
-        Gz = insert_zeros(G, n_zeros)
-        Hz = insert_zeros(H, n_zeros)
+        Gz      = _insert_zeros(G, n_zeros)
+        Hz      = _insert_zeros(H, n_zeros)
         current = (1.0/lambda_j[j])*np.convolve(S,Gz)
         current = current[N_pnts+Gn[j]:2*N_pnts+Gn[j]]
-        prod *= current
-        S_new = np.convolve(S, Hz)
-        S_new = S_new[N_pnts+Hn[j]:2*N_pnts+Hn[j]]
-        S = np.append(S_new[::-1], S_new)
-        S = np.append(S, S_new[::-1])
+        prod    *= current
+        S_new   = np.convolve(S, Hz)
+        S_new   = S_new[N_pnts+Hn[j]:2*N_pnts+Hn[j]]
+        S       = np.append(S_new[::-1], S_new)
+        S       = np.append(S, S_new[::-1])
     return prod
 
 
 
-def insert_zeros(x, n):
-    newlen  = (n+1)*x.size
-    out     = np.zeros(newlen)
-    indices = range(0, newlen-n, n+1)
+def _insert_zeros(x, n):
+    """
+    Helper function for mz_fwt. Splits input array and adds n zeros
+    between values.
+    """
+    newlen       = (n+1)*x.size
+    out          = np.zeros(newlen)
+    indices      = range(0, newlen-n, n+1)
     out[indices] = x
     return out
 
 
 
 def find_steps(array, threshold):
-    steps = []
+    """
+    Finds local maxima by segmenting array based on positions at which
+    the threshold value is crossed. Note that this thresholding is 
+    applied after the absolute value of the array is taken. Thus,
+    the distinction between upward and downward steps is lost. However,
+    get_step_sizes can be used to determine directionality after the
+    fact.
+
+    Parameters
+    ----------
+    array : numpy array
+        1 dimensional array that represents time series of data points
+    threshold : int / float
+        Threshold value that defines a step
+
+
+    Returns
+    -------
+    steps : list
+        List of indices of the detected steps
+
+    """
+    steps        = []
+    array        = np.abs(array)
     above_points = np.where(array > threshold, 1, 0)
-    ap_dif = np.diff(above_points)
-    cross_ups = np.where(ap_dif == 1)[0]
-    cross_dns = np.where(ap_dif == -1)[0]
+    ap_dif       = np.diff(above_points)
+    cross_ups    = np.where(ap_dif == 1)[0]
+    cross_dns    = np.where(ap_dif == -1)[0]
     for upi, dni in zip(cross_ups,cross_dns):
         steps.append(np.argmax(array[upi:dni]) + upi)
     return steps
@@ -126,10 +184,39 @@ def find_steps(array, threshold):
 
 
 def get_step_sizes(array, indices, window=1000):
+    """
+    Calculates step size for each index within the supplied list. Step
+    size is determined by averaging over a range of points (specified
+    by the window parameter) before and after the index of step
+    occurrence. The directionality of the step is reflected by the sign
+    of the step size (i.e. a positive value indicates an upward step,
+    and a negative value indicates a downward step). The combined 
+    standard deviation of both measurements (as a measure of uncertainty
+    in step calculation) is also provided.
+
+    Parameters
+    ----------
+    array : numpy array
+        1 dimensional array that represents time series of data points
+    indices : list
+        List of indices of the detected steps (as provided by 
+        find_steps, for example)
+    window : int, optional
+        Number of points to average over to determine baseline levels
+        before and after step.
+
+
+    Returns
+    -------
+    step_sizes : list
+        List of the calculated sizes of each step
+    step_error : list
+
+    """
     step_sizes = []
     step_error = []
-    indices = sorted(indices)
-    last = len(indices) - 1
+    indices    = sorted(indices)
+    last       = len(indices) - 1
     for i, index in enumerate(indices):
         if i == 0:
             q = min(window, indices[i+1]-index)
